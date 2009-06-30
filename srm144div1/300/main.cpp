4b521eeaadf70d4cbbba89802dce40cb98a7a298
Yuto Hayamizu <info@hayamin.com>
2009-06-30 20:16:05 +0900

    300 ok

---
+++ srm144div1/300/main.cpp
@@ -0,0 +1,21 @@
+
+#include "binary_code.cpp"
+
+void print_result(vector<string> str){
+    cout << "{" << str[0] << "," << str[1] << "}" << endl;
+}
+
+int main(int argc, char ** argv){
+
+    BinaryCode bc;
+
+    vector<string> tests;
+    tests.push_back("123210122");
+    tests.push_back("123210120");
+
+    for(int i = 0;i < tests.size();i++){
+        print_result(bc.decode(tests[i]));
+    }
+
+    return 0;
+}
