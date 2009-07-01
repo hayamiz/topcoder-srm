0881d7046f9917219cbf0ac746ead5203f8ea07a
Yuto Hayamizu <info@hayamin.com>
2009-07-02 03:48:50 +0900

    added template file

---
+++ c++-template.cpp
@@ -0,0 +1,34 @@
+
+#line $NEXTLINENUMBER$ "$FILENAME$"
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+
+typedef long long ll;
+typedef unsigned int ui;
+
+#define veceach(v) for(int i = 0;i < (v).size();i++)
+#define veceach2(v,i) for(int i = 0;i < (v).size();i++)
+
+using namespace std;
+class $CLASSNAME$ {
+    public:
+    $RC$ $METHODNAME$($METHODPARMS$) {
+        
+    }
+
+$TESTCODE$
+};
+
+// BEGIN CUT HERE
+int main() {
+$CLASSNAME$ ___test;
+___test.run_test(-1);
+}
+// END CUT HERE
