0b9ebbf3bb3a647d3b95aa1b0ceca1cdbf618765
Yuto Hayamizu <info@hayamin.com>
2009-07-03 01:29:46 +0900

    almost ok

--- c++-template.cpp
+++ c++-template.cpp
@@ -1,20 +1,61 @@
 
 #line $NEXTLINENUMBER$ "$FILENAME$"
-#include <string>
-#include <iostream>
-#include <vector>
-#include <sstream>
+
+#include <algorithm>
+#include <complex>
+#include <exception>
+#include <list>
 #include <stack>
-#include <queue>
-#include <deque>
+#include <bitset>
+#include <csetjmp>
+#include <fstream>
+#include <locale>
+#include <stdexcept>
+#include <cassert>
+#include <csignal>
+#include <functional>
 #include <map>
+#include <strstream>
+#include <cctype>
+#include <cstdarg>
+#include <iomanip>
+#include <memory>
+#include <streambuf>
+#include <cerrno>
+#include <cstddef>
+#include <ios>
+#include <new>
+#include <string>
+#include <cfloat>
+#include <cstdio>
+#include <iosfwd>
+#include <numeric>
+#include <typeinfo>
+#include <ciso646>
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <utility>
+#include <climits>
+#include <cstring>
+#include <istream>
+#include <queue>
+#include <valarray>
+#include <clocale>
+#include <ctime>
+#include <iterator>
 #include <set>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <limits>
+#include <sstream>
 
 typedef long long ll;
 typedef unsigned int ui;
 
-#define veceach(v) for(int i = 0;i < (v).size();i++)
-#define veceach2(v,i) for(int i = 0;i < (v).size();i++)
+#define veceach(v) for(uint i = 0;i < (v).size();i++)
+#define veceach2(v,i) for(uint i = 0;i < (v).size();i++)
 
 using namespace std;
 class $CLASSNAME$ {
